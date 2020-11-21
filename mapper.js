8bb88fb3f8f94e7cc089437403d75db52d212852
msheslow <msheslow@gmail.com>
2020-11-21 18:47:37 -0500

    ugh

--- mapper.js
+++ mapper.js
@@ -112,8 +112,7 @@ async function addRoute(directionsService, directionsDisplay, waypoints) {
         origin: document.getElementById('start').value,
         destination: document.getElementById('end').value,
         travelMode: 'DRIVING',
-        waypoints: waypoints,
-        optimizeWaypoints: true
+        waypoints: optimize= true | waypoints
     },async function(response, status) {
         if (status === 'OK') {
             await directionsDisplay.setDirections(response);
