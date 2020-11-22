35a50cc842d2ee1b0a629e1e3a138646a4c9ae9c
msheslow <msheslow@gmail.com>
2020-11-22 18:48:15 -0500

    iw annn comitit

--- mapper.js
+++ mapper.js
@@ -32,7 +32,7 @@ async function initMap() {
     $('main').on('click', '.autocomplete-box', autocomplete_click_handler);
 
     async function autocomplete_click_handler(event) {
-        /// console.log(this.childElement.childElement.childElement.value);
+        // console.log(this.childElement.childElement.childElement.value);
         console.log(event.currentTarget);
     }
 
