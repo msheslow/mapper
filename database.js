43801fd9a781ce1287ca94fddc722e1e0081f7e6
Ari Singer-Freeman <arisf@live.unc.edu>
2020-11-22 17:46:38 -0500

    messing with express

--- database.js
+++ database.js
@@ -15,8 +15,14 @@ app.use(express.static(__dirname))
 app.use(expressSession({
     name: "mapperSessionCookie",
     secret: ["username", "tripID"],
-    resave: false,
-    saveUninitialized: false
+    resave: false, //not from stackoverflow
+    saveUninitialized: false, //not from stackoverflow
+    key : 'sid',
+    proxy : true, // add this when behind a reverse proxy, if you need secure cookies
+    cookie : {
+        secure : true,
+        maxAge: 5184000000 // 2 months
+    }
 }));
 
 
@@ -62,8 +68,8 @@ app.get('/logout', (req, res) => {
 //returns JSON object with all trips for logged in user
 app.get('/tripids', async (req, res) => {
     if (req.session.username == undefined) {
-        res.send(req.session.username)
-        // res.status(403).send("Unauthorized");
+        // res.send(req.session.username)
+        res.status(403).send("Unauthorized");
         return;
     }
     let result = await getUsersTripNumbers(req.session.username)
