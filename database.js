ec3aa24f00b49e375a83474e10c0d98284eebad5
Ari Singer-Freeman <arisf@live.unc.edu>
2020-11-22 20:07:08 -0500

    t

--- database.js
+++ database.js
@@ -294,7 +294,7 @@ async function addTripStop(tripID, stopID){
     let sqlStopCommand = `INSERT INTO stops VALUES ("${stopID}", "${tripID}")`
     await searchWrapper(`UPDATE citiesAndSites Set Weight = Weight+1 WHERE Name = "${stopID}"`)
     await searchWrapper(sqlStopCommand)
-    return  await searchWrapper(`SELECT * FROM stops WHERE stopID= "${stopID}"AND tripID ="${tripID}" `)
+    return  tripID
 }
 
 
